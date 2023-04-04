3bc5b0878879f892544f25d94b843d9d6b99f256
Pawel Owiesek <Owiesek.p@gmail.com>
2023-04-04 16:32:17 +0200

    added new functionality to page, changed backgr.()

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
 {
     const welcomeMessage = () => {
         console.log("Hello");
-    }
+    };
+
+    const onWindowScroll = () => {
+        const sections = document.querySelectorAll("section");
+        const navLinks = document.querySelectorAll("header nav a");
+        window.onscroll = () => {
+            sections.forEach(section => {
+                const top = window.scrollY;
+                const offset = section.offsetTop - 60;
+                const height = section.offsetHeight;
+                const id = section.getAttribute("id");
+
+                if (top >= offset && top < offset + height) {
+                    navLinks.forEach(link => {
+                        link.classList.remove("active");
+                        document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+                    });
+                };
+            });
+        };
+    };
 
     const changeBackground = () => {
-        const header = document.querySelector(".header");
+        const body = document.querySelector(".body");
         const themeName = document.querySelector(".themeName");
-        header.classList.toggle("dark");
-        themeName.innerText = header.classList.contains("dark") ? "jasny" : "ciemny";
-    }
+        body.classList.toggle("dark");
+        themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+    };
 
     const init = () => {
         const button = document.querySelector(".js-button");
         button.addEventListener("click", changeBackground);
         welcomeMessage();
-    }
+        onWindowScroll();
+    };
     init();
 }
 
