7c662bf889d5da33a84ba1f1df756ea2fb2a19bc
Pawel Owiesek <Owiesek.p@gmail.com>
2023-05-16 11:04:50 +0200

    code formated

--- js/script.js
+++ js/script.js
@@ -1,41 +1,42 @@
 {
-    const welcomeMessage = () => {
-        console.log("Hello");
-    };
+  const welcomeMessage = () => {
+    console.log("Hello");
+  };
 
-    const onWindowScroll = () => {
-        const sections = document.querySelectorAll("section");
-        const navLinks = document.querySelectorAll("header nav a");
-        window.onscroll = () => {
-            sections.forEach(section => {
-                const top = window.scrollY;
-                const offset = section.offsetTop - 130;
-                const height = section.offsetHeight;
-                const id = section.getAttribute("id");
+  const onWindowScroll = () => {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll("header nav a");
+    window.onscroll = () => {
+      sections.forEach((section) => {
+        const top = window.scrollY;
+        const offset = section.offsetTop - 130;
+        const height = section.offsetHeight;
+        const id = section.getAttribute("id");
 
-                if (top >= offset && top < offset + height) {
-                    navLinks.forEach(link => {
-                        link.classList.remove("active");
-                        document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
-                    });
-                };
-            });
-        };
+        if (top >= offset && top < offset + height) {
+          navLinks.forEach((link) => {
+            link.classList.remove("active");
+            document
+              .querySelector("header nav a[href*=" + id + "]")
+              .classList.add("active");
+          });
+        }
+      });
     };
+  };
 
-    const changeBackground = () => {
-        const body = document.querySelector(".body");
-        const themeName = document.querySelector(".themeName");
-        body.classList.toggle("dark");
-        themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
-    };
+  const changeBackground = () => {
+    const body = document.querySelector(".body");
+    const themeName = document.querySelector(".themeName");
+    body.classList.toggle("dark");
+    themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+  };
 
-    const init = () => {
-        const button = document.querySelector(".js-button");
-        button.addEventListener("click", changeBackground);
-        welcomeMessage();
-        onWindowScroll();
-    };
-    init();
+  const init = () => {
+    const button = document.querySelector(".js-button");
+    button.addEventListener("click", changeBackground);
+    welcomeMessage();
+    onWindowScroll();
+  };
+  init();
 }
-
